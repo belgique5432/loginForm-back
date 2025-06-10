ce707b221877f1c9cc3345c8381a15e23ad33566
belgique5432 <seniniezequiel780@gmail.com>
2025-06-10 17:39:26 -0300

    URL de mongo actualizada

--- database/db.py
+++ database/db.py
@@ -2,10 +2,17 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from dotenv import load_dotenv
 
-load_dotenv()
-client: AsyncIOMotorClient = AsyncIOMotorClient(os.getenv("MONGODB_URI", "mongodb://localhost:27017"))
-db = client["belmulDataBase"]
+load_dotenv(dotenv_path=".env", override=True)
 
+MONGO_URI = os.getenv("MYAPP_MONGO_URI")
+if not MONGO_URI:
+    raise ValueError("La variable de entorno MYAPP_MONGO_URI no está definida")
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["belgi"]
+
+print("MONGO_URI =", MONGO_URI)
+print("CLIENT:", client)
 
 def get_users_db() -> AsyncIOMotorCollection:
     return db["users"]
