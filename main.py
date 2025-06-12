4074662d3c20f1f1d26cd0f15aa2ac9282e7ba42
belgique5432 <seniniezequiel780@gmail.com>
2025-06-12 11:52:33 -0300

    actualizacion bcrypt

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     
-    allow_origins=[       "http://localhost:5173",  # Desarrollo
-        "https://login-form-front-theta.vercel.app/" ] # Producción],  # o ["http://localhost:5173"]
+    allow_origins=["http://localhost:5173",  # Desarrollo
+        "https://login-form-front-theta.vercel.app/"] # Producción],  # o ["http://localhost:5173"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
