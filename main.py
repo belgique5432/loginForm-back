e817895038b62012561a7a7d8f6de4af4baa7820
belgique5432 <seniniezequiel780@gmail.com>
2025-06-12 12:27:49 -0300

    actualizacion de cors

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ app = FastAPI()
 # Agregás el middleware directamente
 app.add_middleware(
     CORSMiddleware,
-    
-    allow_origins=["http://localhost:5173",  # Desarrollo
-        "https://login-form-front-theta.vercel.app/"] # Producción],  # o ["http://localhost:5173"]
+    allow_origins=[
+        "http://localhost:5173",  # Desarrollo
+        "https://login-form-front-theta.vercel.app/"  # Producción
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
